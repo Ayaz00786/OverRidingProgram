ec6743fb9d6fc4a084719819689dac65341d0ad8
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-05-08 22:58:38 +0530

    Add project files.

---
+++ OverRidingProgram/Overriding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverRidingProgram
+{
+    public class Employee
+    {
+         public int id { get;set;}
+         public string FirstName { get;set;}
+        public string LastName { get;set;}
+         public string email { get;set;}
+
+        public virtual void GetFullName()
+        {
+            Console.WriteLine(FirstName+" "+LastName);
+        }
+    }
+    public class FullTimeEmployee : Employee
+
+    {
+        public override void GetFullName()
+        {
+            Console.WriteLine(id+" -- "+FirstName+" "+LastName+" -- "+email);
+        }
+      
+    }
+
+    
+}
---
+++ OverRidingProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OverRidingProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new FullTimeEmployee();
+           
+            emp.FirstName = "Ayaz";
+            emp.LastName = "Ahmad";
+            emp.email = "Ayaj123@gmail.com";
+            emp.id = 101;
+
+           emp.GetFullName();
+
+         
+        }
+    }
+}
